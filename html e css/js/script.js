3766867b5b2ccc66ee12c911c097bf6a4a5fd499
ArtFiorindo <alopesfiorindo@gmail.com>
2023-11-27 17:28:07 -0300

    Carrossel pronto, cadastro cliente finalizado.

--- html e css/js/script.js
+++ html e css/js/script.js
@@ -1,17 +1,27 @@
-let count = 1
-document.getElementById("radio1").checked = true;
+const slider = document.querySelector('.gallery');
+let isDown = false;
+let startX;
+let scrollLeft;
 
-setInterval( function(){
-    nextImage()
-},3500 )
-
-
-function nextImage(){
-    count++;
-    if(count>4){
-        count = 1;
-    }
-
-    document.getElementById("radio"+count).checked = true;
-
-}
+slider.addEventListener('mousedown', e => {
+  isDown = true;
+  slider.classList.add('active');
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+});
+slider.addEventListener('mouseleave', _ => {
+  isDown = false;
+  slider.classList.remove('active');
+});
+slider.addEventListener('mouseup', _ => {
+  isDown = false;
+  slider.classList.remove('active');
+});
+slider.addEventListener('mousemove', e => {
+  if (!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  const SCROLL_SPEED = 3;
+  const walk = (x - startX) * SCROLL_SPEED;
+  slider.scrollLeft = scrollLeft - walk;
+});
