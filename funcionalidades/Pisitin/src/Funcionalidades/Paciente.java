fb126abaa798ff831ba219523d5dd9e04ddb91b6
Eduardo Função <45571086+eduardofuncao@users.noreply.github.com>
2023-11-28 00:08:09 -0300

    adiciona atributos, construtores, getters e setters para as classes Usuario, Paciente e Psicologo

--- funcionalidades/Pisitin/src/Funcionalidades/Paciente.java
+++ funcionalidades/Pisitin/src/Funcionalidades/Paciente.java
@@ -1,5 +1,34 @@
 package Funcionalidades;
 
-public class Paciente {
-
+public class Paciente extends Usuario{
+	private String idPaciente;
+	private String planoSaude;
+	
+	
+	
+	public Paciente() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Paciente(String idUser, String nome, String email, String telefone, String cidade, String sexo, String bio, String idPaciente, String planoSaude) {
+		super(idUser, nome, email, telefone, cidade, sexo, bio);
+		this.idPaciente = idPaciente;
+		this.planoSaude = planoSaude;
+	}
+	public String getIdPaciente() {
+		return idPaciente;
+	}
+	public void setIdPaciente(String idPaciente) {
+		this.idPaciente = idPaciente;
+	}
+	public String getPlanoSaude() {
+		return planoSaude;
+	}
+	public void setPlanoSaude(String planoSaude) {
+		this.planoSaude = planoSaude;
+	}
+	
+	
+	
+	
 }
--- funcionalidades/Pisitin/src/Funcionalidades/Usuario.java
+++ funcionalidades/Pisitin/src/Funcionalidades/Usuario.java
@@ -1,6 +1,83 @@
 package Funcionalidades;
 
 public class Usuario {
-	private int nome;
-	
+    private String idUser;
+    private String nome;
+    private String email;
+    private String telefone;
+    private String cidade;
+    private String sexo;
+    private String bio;
+    
+    public Usuario() {
+    	
+    }
+    
+	public Usuario(String idUser, String nome, String email, String telefone, String cidade, String sexo, String bio) {
+		this.idUser = idUser;
+		this.nome = nome;
+		this.email = email;
+		this.telefone = telefone;
+		this.cidade = cidade;
+		this.sexo = sexo;
+		this.bio = bio;
+	}
+
+	public String getIdUser() {
+		return idUser;
+	}
+
+	public void setIdUser(String idUser) {
+		this.idUser = idUser;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public String getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+
+	public String getBio() {
+		return bio;
+	}
+
+	public void setBio(String bio) {
+		this.bio = bio;
+	}
+    
+    
 }
