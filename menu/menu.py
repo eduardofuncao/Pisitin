8bc781c45b3e07a587b6f299ca9259fa22a93a5f
ArtFiorindo <alopesfiorindo@gmail.com>
2023-11-28 18:33:10 -0300

    cadastro atualizado fizalizado

--- menu/menu.py
+++ menu/menu.py
@@ -8,21 +8,32 @@ Menu de funcionalidades para o portal Pisitin
 Funcionalidades implementadas:
 A DEFINIR
 '''
+nome = []
+região = []
 
 stop = False
 while not stop:
     print('''
     Menu de funcionalidades:
-    1. Opção 1
-    2. 1. Opção 2
-    3. 1. Opção 3
-    4. 1. Opção 4
+    1. Opção 1 - Cadastro Paciente
+    2. 1. Opção 2 - Cadastro Psicologo
+    3. 1. Opção 3 - informações da conta
+    4. 1. Opção 4 - Contas que combinam com você!
     5. Sair
 ''')
     # seleção do usuário
     sel = int(input("Digite o código da funcionalidade para acessar as suas funções: "))
     if sel == 1:
-        print("opção 1")
+        print("Login")
+        nome = input("Digite o seu nome: ")
+        senha = input("Digite uma senha: ")
+        região = input("Digite a sua região: ")
+
+
+        print("Sucesso ao entrar na conta!")
+
+        input()
+
 
     elif sel == 2:
         print("opção 2")
