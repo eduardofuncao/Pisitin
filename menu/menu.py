6d6e7a0f7047c7491a60b8301f7fa81b80b5f35d
ArtFiorindo <alopesfiorindo@gmail.com>
2023-11-30 02:27:45 -0300

    Menu em python funcionando!

--- menu/menu.py
+++ menu/menu.py
@@ -8,62 +8,71 @@ Menu de funcionalidades para o portal Pisitin
 Funcionalidades implementadas:
 A DEFINIR
 '''
-nome = []
-região = []
-area = []
+
+
+nome_pac = []
+região_pac = []
+area_pac = []
+
+nome_psi = []
+região_psi = []
+area_psi = []
+
+qntd_pac = 0
+qntd_psi = 0
 
 stop = False
 while not stop:
     print('''
     Menu de funcionalidades:
-    1. Opção 1 - Cadastro Psicologo
-    2. 1. Opção 2 - Cadastro Paciente
-    3. 1. Opção 3 - informações da conta
-    4. 1. Opção 4 - Contas que combinam com você!
+    1. Opção 1 - Cadastro Psicólogo
+    2. Opção 2 - Cadastro Paciente
+    3. Opção 3 - Informações da conta
+    4. Opção 4 - Contas que combinam com você!
     5. Sair
 ''')
-    # seleção do usuário
     sel = int(input("Digite o código da funcionalidade para acessar as suas funções: "))
-    while((sel<=0) or (sel>=5)):
-        print('Erro! digite um numero que esteja na lista de opções.')
+    while not (1 <= sel <= 4):
+        print('Erro! Digite um número que esteja na lista de opções.')
         sel = int(input("Digite o código da funcionalidade para acessar as suas funções: "))
-    
-
 
     if sel == 1:
         print("----Cadastro Psicólogo----")
-        nome = input("Digite o seu nome: ")
-        senha = input("Digite uma senha: ")
-        região = input("Digite a sua região: ")
-        CFP = int(input("Digite o seu CFP, Cadastro Nacional de Psicólogas(os):"))
-        area = int(input("Escolha a sua área de atuação:\n 1-Psicanálise\n 2-Psicologia Analítica\n 3-Cognitivo-Comportamental:"))
-
-
+        nome_psi.append(input("Digite o seu nome: "))
+        senha_psi = input("Digite uma senha: ")
+        região_psi.append(input("Digite a sua região: "))
+        CFP_psi = int(input("Digite o seu CFP, Cadastro Nacional de Psicólogas(os):"))
+        area_psi.append(int(input("Escolha a sua área de atuação:\n 1-Psicanálise\n 2-Psicologia Analítica\n 3-Cognitivo-Comportamental:")))
+        qntd_psi += 1
         print("Sucesso ao se cadastrar!")
-
-        input()
-
 
     elif sel == 2:
         print("----Cadastro Paciente----")
-        nome = input("Digite o seu nome: ")
-        senha = input("Digite uma senha: ")
-        região = input("Digite a sua região: ")
-        area = int(input("Escolha a sua área de atuação:\n 1-Psicanálise\n 2-Psicologia Analítica\n 3-Cognitivo-Comportamental:"))
+        nome_pac.append(input("Digite o seu nome: "))
+        senha_pac = input("Digite uma senha: ")
+        região_pac.append(input("Digite a sua região: "))
+        area_pac.append(int(input("Escolha a sua área de atuação:\n 1-Psicanálise\n 2-Psicologia Analítica\n 3-Cognitivo-Comportamental:")))
+        qntd_pac += 1
+        print("Sucesso ao se cadastrar!")
 
-        print("Sucesso")
-
-    
     elif sel == 3:
         print("Informações da conta")
-        print(f"Nome: {nome} | região: {região} | área escolhida: {area}")
-
+        for i in range(qntd_pac):
+            print(f"Nome paciente: {nome_pac[i]} | Região paciente: {região_pac[i]} | Área escolhida paciente: {area_pac[i]}")
+        for i in range(qntd_psi):
+            print(f"Nome psicólogo: {nome_psi[i]} | Região psicólogo: {região_psi[i]} | Área escolhida psicólogo: {area_psi[i]}")
 
     elif sel == 4:
-        print("opção 4")
+        print("Contas que combinam com você!")
+        for i in range(qntd_pac):
+            for j in range(qntd_psi):
+                if (região_pac[i] == região_psi[j]) and (area_pac[i] == area_psi[j]):
+                    print(f"Nome paciente: {nome_pac[i]} | Região paciente: {região_pac[i]} | Área escolhida paciente: {area_pac[i]}")
+                    print(f"Nome psicólogo: {nome_psi[j]} | Região psicólogo: {região_psi[j]} | Área escolhida psicólogo: {area_psi[j]}")
 
     elif sel == 5:
-        print("Saindo do programa...")
-        stop = True
+        print("saiu do programa")
 
-print("Fim do programa!")
+
+
+
